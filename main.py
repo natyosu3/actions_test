85cbefa6d53c37fe8e7156787cb79eece687689c
natyosu3 <nachi334825@gmail.com>
2023-10-14 20:39:08 +0900

    update

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import discord
+import os
 
-print("hello, World!")
-print(discord)
+
+TOKEN = str(os.environ.get("TOKEN"))
+GUILD_ID = 1117469059957665942
+Echo_ID = 1059820902755344473
+bot = discord.Client(intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("pingを確認")
+    echo: discord.Member = bot.get_guild(GUILD_ID).get_member(Echo_ID)
+
+    print(echo.status)
+    await bot.close()
+
+
+if __name__ == '__main__':
+    bot.run(TOKEN)
