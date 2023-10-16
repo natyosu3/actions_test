dabe2edddef6138fbd4580f645f34c908f8fac47
natyosu3 <nachi334825@gmail.com>
2023-10-16 15:59:03 +0900

    update

--- main.py
+++ main.py
@@ -15,18 +15,34 @@ async def on_ready():
     echo: discord.Member = bot.get_guild(GUILD_ID).get_member(Echo_ID)
     ch: discord.TextChannel = bot.get_channel(CH_ID)
 
-    if str(echo.status) == "online":
-        await ch.send(embed=discord.Embed(
+    status_msg: discord.Message = await ch.fetch_message(1163366925661917234)
+
+    if str(echo.status) == "online" and status_msg != None:
+        await status_msg.edit(embed=discord.Embed(
             title="Echo BOT Status",
             color=discord.Color.green(),
             description="```python\nOnline!```"
         ))
     else:
-        await ch.send(embed=discord.Embed(
+        await status_msg.edit(embed=discord.Embed(
             title="Echo BOT Status",
             color=discord.Color.red(),
             description="```python\nOffline!\n Echo is downing...```"
         ))
+
+    if status_msg == None:
+        if str(echo.status) == "online":
+            await status_msg.edit(embed=discord.Embed(
+                title="Echo BOT Status",
+                color=discord.Color.green(),
+                description="```python\nOnline!```"
+            ))
+        else:
+            await status_msg.edit(embed=discord.Embed(
+                title="Echo BOT Status",
+                color=discord.Color.red(),
+                description="```python\nOffline!\n Echo is downing...```"
+            ))
 
     await bot.close()
 
