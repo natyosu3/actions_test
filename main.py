1ef46ef94fae9a6e940f30bcf1767579cdc5b444
natyosu3 <nachi334825@gmail.com>
2023-10-16 15:41:26 +0900

    埋め込みテキストを送信する処理の追加

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 
 TOKEN = str(os.environ.get("TOKEN"))
 GUILD_ID = 1117469059957665942
+
+CH_ID = 913410359711375410
+
 Echo_ID = 1059820902755344473
 bot = discord.Client(intents=discord.Intents.all())
 
@@ -12,8 +15,21 @@ async def on_ready():
     print("pingを確認")
     echo: discord.Member = bot.get_guild(GUILD_ID).get_member(Echo_ID)
 
+    ch: discord.TextChannel = bot.get_channel(CH_ID)
 
-    print(echo.status)
+    if str(echo.status) == "online":
+        await ch.send(embed=discord.Embed(
+            title="Echo BOT Status",
+            color=discord.Color.green(),
+            description="```python online!```"
+        ))
+    else:
+        await ch.send(embed=discord.Embed(
+            title="Echo BOT Status",
+            color=discord.Color.red(),
+            description="```python offline!\n Echo is downing...```"
+        ))
+
     await bot.close()
 
 
